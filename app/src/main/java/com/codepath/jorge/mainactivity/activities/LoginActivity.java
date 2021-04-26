969e43206e998307cb18c104e658e42e9db1b86d
erikfors <efors003@fiu.edu>
2021-04-26 16:48:08 -0400

    Change style

--- app/src/main/java/com/codepath/jorge/mainactivity/activities/LoginActivity.java
+++ app/src/main/java/com/codepath/jorge/mainactivity/activities/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private ImageView imageBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,10 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        imageBackground = findViewById(R.id.imageBackgroundLogin);
+
+        Glide.with(this).load(R.drawable.giphy).into(imageBackground);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
 
             @Override
